4d87194c8f54b9f2c128f5f382802f46ab155016
Christian Kopac <hello@christiankopac.com>
2024-07-22 19:11:02 +0200

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,62 +1,96 @@
+import XIcon from "@mui/icons-material/X";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+
 const Footer = () => {
   return (
     <footer>
-      {/* logo */}
-      <div>
+      <div className="footer--logo">
         <img src="/logo.png" alt="Little Lemon Logo" />
       </div>
-      {/* navigation */}
-      <div>
-        <h2>Explore</h2>
+      <div className="footer--nav">
+        <h2>Navigation</h2>
         <nav>
           <ul>
             <li>
               <a href="/">Home</a>
             </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Reservations</a>
+            </li>
+            <li>
+              <a href="/">Order Online</a>
+            </li>
           </ul>
         </nav>
       </div>
-      {/* contact */}
-      <div>
-        <h2>Visit Us</h2>
+      <div className="footer--contact">
+        <h2>Contact</h2>
         <p>Little Lemon</p>
-        <p>123 Main St</p>
-        <p>Springfield, IL 62701</p>
-        <p>United States</p>
+        <address>
+          <p>123 Main St</p>
+          <p>Springfield, IL 62701</p>
+          <p>United States</p>
+        </address>
         {/* add phone icon */}
-        <p>123-456-7890</p>
-        {/* add email icon */}
         <p>
-          <a href="mailto:example@littlelemon.com">Email Us</a>
+          <a href="tel:+1234567890" className="link--icon">
+            <LocalPhoneIcon />
+            +1 (234) 567-890
+          </a>
+        </p>
+        <p>
+          <a href="mailto:example@littlelemon.com" className="link--icon">
+            <EmailIcon />
+            Email Us
+          </a>
         </p>
       </div>
-      {/* social media */}
-      <div>
-        <h2>Follow Us</h2>
-        <a
-          href="https://www.facebook.com/littlelemon"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {/* add facebook icon */}
-          <span>Facebook</span>
-        </a>
-        <a
-          href="https://www.instagram.com/littlelemon"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {/* add instagram icon */}
-          <span>Instagram</span>
-        </a>
-        <a
-          href="https://www.twitter.com/littlelemon"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {/* add twitter icon */}
-          <span>Twitter</span>
-        </a>
+      <div className="footer--social">
+        <h2>Social Media</h2>
+        <ul>
+          <li>
+            <a
+              className="link--icon"
+              href="https://www.facebook.com/littlelemon"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FacebookIcon />
+              <span>Facebook</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="link--icon"
+              href="https://www.instagram.com/littlelemon"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <InstagramIcon />
+              <span>Instagram</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="link--icon"
+              href="https://www.twitter.com/littlelemon"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <XIcon />
+              <span>Twitter</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,42 @@
+import logo from "../images/logo.svg";
+
+const styles = {};
+
 const Nav = () => {
   return (
-    <nav>
-      <ul>
+    <nav style={styles.navigation}>
+      {/* logo */}
+      <a href="/">
+        <img src={logo} alt="Little Lemon Logo" />
+      </a>
+      {/* mobile navigation icon */}
+      <div className="hamburger">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      {/* navigation items */}
+      <ul style={styles.navigationItems}>
         <li>
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="/">About</a>
         </li>
         <li>
-          <a href="#services">Services</a>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
         </li>
       </ul>
     </nav>
